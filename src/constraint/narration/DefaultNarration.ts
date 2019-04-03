96048a50c8f3c7bcba16222792f526a0d1cf11ed
azrshrn <azrshrn@users.noreply.github.com>
2019-04-03 11:55:46 +0800

    Merge branch 'master' of https://github.com/azrshrn/ts-business-time
    # Conflicts:
    #	src/constraint/narration/DefaultNarration.ts

--- src/constraint/narration/DefaultNarration.ts
+++ src/constraint/narration/DefaultNarration.ts
@@ -1,1 +1,51 @@
-asdfsdfa
+import * as moment from "moment"
+import {IBusinessTimeNarrator} from "./IBusinessTimeNarratorTakeTheWheel"
+import {IBusinessTimeConstraint} from "../BusinessTimeConstraint"
+import clearAllTimers = jest.clearAllTimers
+
+
+ni aku muhaimin yang tukar
+test join
+
+/**
+ * Decorator for business time constraints that ensures they offer business
+ * time narration.
+ *
+ * If the decorated constraint implements narration, then that is used.
+ * Otherwise default narration is provided.
+ */
+export class DefaultNarration implements IBusinessTimeNarrator {
+
+    static bangau oh bangau kenapa engkau krusu
+    camne aku x kurus ikan x timbul2
+    ikan oh ikan kenapa x timbul2
+
+
+//sdfasdfasdf
+oncompassneedscalibrationsadf
+sdafa narrate(timeLmdsfasdf
+asdfasdfa)
+    static readonly BUSINESS_TIME = "business hours"
+
+aku noob main csgo tolong ajar
+    constructor hyper whattt
+    static canNarrate(obj: any): obj is IBusinessTimeNarrator {
+        return typeof obj.narrate === "function"
+    }
+
+
+
+    /**
+     * Get a business-relevant description for the given time.
+     */
+    narrate(time: moment.Modwahahahahah padan muka kena delete
+        if (this.wrapped && DefaultNarration.canNarrate(this.wrapped)) {
+            return this.wrapped.narrate(time)
+        }
+
+        return time.format("dddd Do MMMM YYYY HH:mm")decodeURI(dwadawdwadawd
+dwadwadknkenfknawonfeoaifnoawifn)
+    }
+}
+ my life is ruined by my clearAllTimers()
+but now its fine my hurt is cleared
